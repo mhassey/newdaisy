51a4b5dee50262da7ed05b90f5ace6c436200951
prateek <prateek.gupta@appventurez.com>
2021-07-07 10:04:54 +0530

    small changes

--- app/src/main/java/com/daisy/service/DeletePhotoService.java
+++ app/src/main/java/com/daisy/service/DeletePhotoService.java
@@ -28,7 +28,7 @@ public class DeletePhotoService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         sessionManager = SessionManager.get();
         if (sessionManager.getDeletePhoto()) {
-           Utils.deleteGalaryPhoto();
+          Utils.deleteGalaryPhoto();
         }
         Utils.deleteCallList(getApplicationContext());
         stopSelf();
