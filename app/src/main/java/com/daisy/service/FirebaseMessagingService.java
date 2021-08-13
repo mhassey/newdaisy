d4a00e962c4bf3121de5f01344c0dec687611a66
prateek <prateek.gupta@appventurez.com>
2021-08-13 16:12:56 +0530

    NZ server modification

--- app/src/main/java/com/daisy/service/FirebaseMessagingService.java
+++ app/src/main/java/com/daisy/service/FirebaseMessagingService.java
@@ -10,8 +10,8 @@ import com.google.firebase.messaging.RemoteMessage;
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-       // CheckCardAvailability checkCardAvailability = new CheckCardAvailability();
-       // checkCardAvailability.checkCard();
+//        CheckCardAvailability checkCardAvailability = new CheckCardAvailability();
+//        checkCardAvailability.checkCard();
         super.onMessageReceived(remoteMessage);
     }
 }
