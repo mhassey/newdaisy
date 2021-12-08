f410ebd1612a58650c8e3e503951d6ce01b105b4
Prateek <prateek.gupta@appventurez.com>
2021-12-08 10:54:26 +0530

    bug fixes

--- app/src/main/java/com/daisy/activity/onBoarding/slider/screenAdd/ScreenAddValidationHelper.java
+++ app/src/main/java/com/daisy/activity/onBoarding/slider/screenAdd/ScreenAddValidationHelper.java
@@ -6,17 +6,21 @@ import com.daisy.R;
 import com.daisy.databinding.AddScreenBinding;
 import com.daisy.utils.ValidationHelper;
 
-public class ScreenAddValidationHelper  {
+public class ScreenAddValidationHelper {
     private Context context;
     private AddScreenBinding binding;
-    public ScreenAddValidationHelper(Context context,AddScreenBinding binding)
-    {
-        this.context=context;
-        this.binding=binding;
+
+    public ScreenAddValidationHelper(Context context, AddScreenBinding binding) {
+        this.context = context;
+        this.binding = binding;
     }
 
-    public boolean isValid()
-    {
-        return  true;
+    public boolean isValid() {
+        if (binding.productName.getSelectedItem() != null)
+            return true;
+        else {
+            ValidationHelper.showToast(context, context.getString(R.string.please_select_product));
+            return false;
+        }
     }
 }
