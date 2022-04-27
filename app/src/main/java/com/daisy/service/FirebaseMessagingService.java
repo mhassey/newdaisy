a8a9c0eab926faf94d7284d988fb0e91f3fd79f4
Prateek <prateek.gupta@appventurez.com>
2022-04-27 15:16:24 +0530

    bug fixes

--- app/src/main/java/com/daisy/service/FirebaseMessagingService.java
+++ app/src/main/java/com/daisy/service/FirebaseMessagingService.java
@@ -10,8 +10,9 @@ import com.google.firebase.messaging.RemoteMessage;
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-       // CheckCardAvailability checkCardAvailability = new CheckCardAvailability();
-       // checkCardAvailability.checkCard();
+        Log.e("kali", "push comming");
+        // CheckCardAvailability checkCardAvailability = new CheckCardAvailability();
+        // checkCardAvailability.checkCard();
         super.onMessageReceived(remoteMessage);
     }
 }
