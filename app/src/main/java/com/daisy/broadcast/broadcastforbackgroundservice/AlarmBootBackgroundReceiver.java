a0bb2de53518cb58109dd67b8f7e0b085565b0e1
Prateek <prateek.gupta@appventurez.com>
2022-11-18 11:22:53 +0530

    remove unused  code

--- app/src/main/java/com/daisy/broadcast/broadcastforbackgroundservice/AlarmBootBackgroundReceiver.java
+++ app/src/main/java/com/daisy/broadcast/broadcastforbackgroundservice/AlarmBootBackgroundReceiver.java
@@ -6,13 +6,11 @@ import android.content.Intent;
 
 import com.daisy.activity.mainActivity.MainActivity;
 import com.daisy.common.session.SessionManager;
-import com.daisy.utils.ValidationHelper;
 
 
 public class AlarmBootBackgroundReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        ValidationHelper.showToast(context, "dhusuadshuidas");
         if (!SessionManager.get().getLogout()) {
             if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED") || intent.getAction().equals("android.intent.action.QUICKBOOT_POWERON")) {
                 try {
@@ -24,7 +22,6 @@ public class AlarmBootBackgroundReceiver extends BroadcastReceiver {
 
 
             }
-            ValidationHelper.showToast(context, "dhusua0------------dshuidas");
 
             Intent intent2 = new Intent(context, MainActivity.class);
             intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
