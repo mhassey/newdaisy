3564210b76b9d11d7f6aedbb719aaa7177745771
mac <prateek.gupta@appventurez.com>
2022-11-29 15:07:08 +0530

    dynamic widget

--- app/src/main/java/com/iris/widget/PriceCardWidget.java
+++ app/src/main/java/com/iris/widget/PriceCardWidget.java
@@ -7,9 +7,13 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.SizeF;
 import android.widget.RemoteViews;
 
 import com.iris.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Implementation of App Widget functionality.
@@ -19,8 +23,6 @@ public class PriceCardWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.price_card_widget);
 //        views.setTextViewText(R.id.appwidget_text, widgetText);
 
@@ -30,9 +32,16 @@ public class PriceCardWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Map<SizeF, RemoteViews> viewMapping = new HashMap();
+        viewMapping.put(new SizeF(100f, 50f), new RemoteViews(context.getPackageName(), R.layout.price_card_widget));
+        viewMapping.put(new SizeF(150f, 50f), new RemoteViews(context.getPackageName(), R.layout.price_card_widget_large));
+        viewMapping.put(new SizeF(215f, 50f), new RemoteViews(context.getPackageName(), R.layout.price_card_widget_medium));
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            RemoteViews remoteViews = new RemoteViews(viewMapping);
+            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
 
+        }
     }
-
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
