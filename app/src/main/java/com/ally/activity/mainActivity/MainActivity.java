6628f870bef1ebda5c7c62741347a41e24605e2b
mac <prateek.gupta@appventurez.com>
2022-12-13 17:05:39 +0530

    work on ally issues

--- app/src/main/java/com/ally/activity/mainActivity/MainActivity.java
+++ app/src/main/java/com/ally/activity/mainActivity/MainActivity.java
@@ -121,6 +121,7 @@ public class MainActivity extends BaseActivity implements CallBack, View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     /**
