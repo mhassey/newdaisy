4f5b03357126cc4284b6c48157ddf729090d3d48
mac <prateek.gupta@appventurez.com>
2023-01-05 15:53:23 +0530

    work on issues

---
+++ app/src/main/java/com/daisy/utils/fgchecker/Utils.java
@@ -0,0 +1,25 @@
+package com.daisy.utils.fgchecker;
+
+import android.annotation.TargetApi;
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.os.Build;
+
+public class Utils {
+    private Utils() {
+
+    }
+
+    public static boolean postLollipop() {
+        return true;
+    }
+
+    public static boolean hasUsageStatsPermission(Context context) {
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow("android:get_usage_stats",
+                android.os.Process.myUid(), context.getPackageName());
+        boolean granted = mode == AppOpsManager.MODE_ALLOWED;
+        return granted;
+    }
+
+}
---
+++ app/src/main/java/com/daisy/utils/fgchecker/detectors/Detector.java
@@ -0,0 +1,8 @@
+package com.daisy.utils.fgchecker.detectors;
+
+
+import android.content.Context;
+
+public interface Detector {
+    String getForegroundApp(Context context);
+}
