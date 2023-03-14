87824ec44ab9f51def63f1ed341ec1bcd1f8ddfe
mac <prateek.gupta@appventurez.com>
2023-03-14 20:20:11 +0530

    work on initial setup

--- app/src/main/java/com/daisy/utils/Constraint.java
+++ app/src/main/java/com/daisy/utils/Constraint.java
@@ -401,9 +401,9 @@ public class Constraint {
     public static String MobilePriceCard = "MobilePriceCard";
     public static long Two_Minute = 60000;
     @NotNull
-    public static final String STORE_CODE_VALUE="10fwd";
+    public static final String STORE_CODE_VALUE="m0t0";
     @NotNull
-    public static final String PASSWORD_VALUE="10fwd";
+    public static final String PASSWORD_VALUE="m0t0";
     @Nullable
     public static final String OPEN_SELECT_PRODUCT="OpenSelectProduct";
 }
